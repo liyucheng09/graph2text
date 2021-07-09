b5e9c0d4a5bffe7b77719996baed012a74836e53
liyucheng <liyucheng_2@qq.com>
2021-07-09 22:36:31 +0800

    update

--- get_oie_tuples.py
+++ get_oie_tuples.py
@@ -4,9 +4,10 @@ import allennlp_models.tagging
 import pickle
 import sys
 import torch
+from tqdm import tqdm
 
 if torch.cuda.is_available():
-    num_gpu = torch.cuda.device_count()
+    num_gpu = 0
 else:
     num_gpu = -1
 
@@ -14,18 +15,20 @@ model_path, output_file, = sys.argv[1:]
 print(f'Model path: {model_path}')
 
 predictor = Predictor.from_path(model_path, cuda_device = num_gpu)
+print('Loaded predictor!')
 ds=load_dataset('xsum')
 processed=[]
-for i in ds['validation']:
-    document = i['document']
-    summary = i['summary']
-    doc_verbs = predictor.predict(sentence=document)['verbs']
-    summary_verbs = predictor.predict(sentence=summary)['verbs']
-    i['doc_verbs'] = doc_verbs
-    i['summary_verbs'] = summary_verbs
-    processed.append(i)
+try:
+    for i in tqdm(ds['validation']):
+        document = i['document']
+        summary = i['summary']
+        doc_verbs = predictor.predict(sentence=document)['verbs']
+        summary_verbs = predictor.predict(sentence=summary)['verbs']
+        i['doc_verbs'] = doc_verbs
+        i['summary_verbs'] = summary_verbs
+        processed.append(i)
+finally:
+    with open(output_file, 'wb') as f:
+        pickle.dump(processed, f)
 
-with open(output_file, 'wb') as f:
-    pickle.dump(processed, f)
-
-print(f'Saved to {output_file}.')
+    print(f'Saved to {output_file}.')
